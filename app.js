63ae805790d9b6d855c06dccd6aa21f41b1a9ede
pcastaneda-tfe <pcastaneda@tfewines.com>
2023-09-14 09:57:35 -0700

    Setup logic to control progress fill line

--- app.js
+++ app.js
@@ -37,5 +37,19 @@ const update = () => {
         } else {
             circle.classList.remove('active');
         }
-    })
-}
+    });
+
+    // get number of active circles
+    const actives = document.querySelectorAll('.active');
+    // convert active circles and cirlces into a percentage
+    progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + '%';
+
+    if(currentActive = 1) {
+        prevBtn.disabled = true;
+    } else if (currentActive === circles.length) {
+        nextBtn.disabled = true;
+    } else {
+        prevBtn.disabled = true;
+        nextBtn.disabled = true;
+    }
+};
